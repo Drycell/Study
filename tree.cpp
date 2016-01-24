516913f1f99843a128b917e5593b5b672b80c758
The drycell <Drycell@jeonjihwan-ui-MacBook-Pro.local>
2016-01-24 23:42:25 +0900

    tree

--- tree.cpp
+++ tree.cpp
@@ -3,37 +3,100 @@
 struct Node
 {
 	int data;
-	Node *left;
-	Node *right;
+	Node *Left;
+	Node *Right;
 };
 
 class Bitree
 {
 	private:			
 	public:
-		Bitree();
-		~Bitree();
-		void Create_node();
-		void Connect_left();
-		void Connect_right();
-		void Input_data();		
+		Node *Create_node();
+		void Connect_left(Node *pa, Node *ch);
+		void Connect_right(Node *pa, Node *ch);
+		void Pre(Node *node);
+		void In(Node *node);
+		void Post(Node *node);
 };
 
 int main(void)
 {
+	Bitree bitree;
+	Node **node = new Node*[8];
+
+	for(int i = 1; i < 8; i++)
+	{
+		node[i] = bitree.Create_node();
+		node[i] -> data = i;
+	}
+
+	for(int i = 1; i < 4; i++)
+	{
+		bitree.Connect_left(node[i], node[2*i]);
+		bitree.Connect_right(node[i], node[2*i + 1]);
+	}
+
+	std::cout << "Pre : ";
+	bitree.Pre(node[1]);
+	std::cout << std::endl;
+
+	std::cout << "Post : ";
+	bitree.Post(node[1]);
+	std::cout << std::endl;
+	
+	std::cout << "In : ";
+	bitree.In(node[1]);
+	std::cout << std::endl;
+	
+	delete []node;
+
 	return 0;
 }
 
-Bitree::Bitree()
+Node *Bitree::Create_node()
 {
-	
+	Node *NewNode = new Node;
+	NewNode -> Left = nullptr;
+	NewNode -> Right = nullptr;
+	return NewNode;
 }
 
-void Bitree::Create_node()
+void Bitree::Connect_left(Node *pa, Node *ch)
 {
-	Node *Newnode = new Node;
-	NewNode -> left = nullptr;
-	NewNode -> right = nullptr;
+	pa -> Left = ch;
 }
 
-void Bitree::
+void Bitree::Connect_right(Node *pa, Node *ch)
+{
+	pa -> Right = ch;
+}
+
+void Bitree::Pre(Node *node)
+{
+	if(node == nullptr)
+		return;
+
+	std::cout << node -> data << " ";
+	Pre(node -> Left);
+	Pre(node -> Right);
+}
+
+void Bitree::Post(Node *node)
+{
+	if(node == nullptr)
+		return;
+
+	Post(node -> Left);
+	Post(node -> Right);
+	std::cout << node -> data << " ";
+}
+
+void Bitree::In(Node *node)
+{
+	if(node == nullptr)
+		return;
+
+	In(node -> Left);
+	std::cout << node -> data << " ";
+	In(node -> Right);
+}
